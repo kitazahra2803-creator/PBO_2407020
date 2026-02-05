9aa55eec49c3efd96adac507a0461888a7eaaeda
kitazahra2803-creator <kitazahra2803@gmail.com>
2026-02-05 09:33:53 +0700

    praktikum 1

---
+++ latihan_2407020/src/latihan_pbo/Main.java
@@ -0,0 +1,9 @@
+package latihan_pbo;
+
+public class Main {
+
+                    public static void main(String[] args) {
+                                        System.out.println("praktikum 1");
+                    }
+                    
+}
