82b29a1cb2eb7423cd52ea75c56e6375bb8d4caf
kitazahra2803-creator <kitazahra2803@gmail.com>
2026-02-20 14:59:23 +0700

    commit pertama

--- latihan_2407020/src/latihan_pbo/Main.java
+++
@@ -1,9 +0,0 @@
-package latihan_pbo;
-
-public class Main {
-
-                    public static void main(String[] args) {
-                                        System.out.println("praktikum 1");
-                    }
-                    
-}
---
+++ latihan_2407020/src/latihan_pbo/latihan2_2407020/Main.java
@@ -0,0 +1,11 @@
+package latihan_pbo.latihan2_2407020;
+
+
+
+public class Main {
+                    public static void main(String[] args) {
+                               
+                                      
+                    }
+}
+
